2b4e4508b1fea99dc549ceb1db03a1184d1c3803
zhoulin <Fancyzero@live.cn>
2017-01-18 14:14:24 +0800

    numerical gradient

--- TrainingSet.py
+++ TrainingSet.py
@@ -7,55 +7,70 @@ image_file = "TrainingSet/train-images.idx3-ubyte"
 
 
 class TrainingSet:
-    count = 0
+    examples = 0
     width = 0
     height = 0
-    class_count = 10
+    classes = 10
 
     def __init__(self):
         f = open(image_file, "rb")
         header = f.read(16)
         header = struct.unpack(">IIII", header)
-        self.count = header[1]
+        self.examples = header[1]
         self.width = header[2]
         self.height = header[3]
-        self.pics = np.fromfile(f, dtype=np.ubyte, count=self.width * self.height * self.count)
+        self.pics = np.fromfile(f, dtype=np.ubyte, count=self.width * self.height * self.examples)
         f.close()
-        self.pics = self.pics.reshape(self.count, self.width, self.height)
+        self.pics = self.pics.reshape(self.examples, self.width * self.height)
         f = open(label_file, "rb")
         f.read(8)
-        self.labels = np.fromfile(f, dtype=np.ubyte, count=self.count)
-        print "loaded " + str(self.count)
-
-    def get_pic(self, index):
-        return self.pics[index]
-
-    def get_label(self, index):
-        return self.labels[index]
-
-xx = np.ndarray((60000,10,748))
+        self.labels = np.fromfile(f, dtype=np.ubyte, count=self.examples)
+        print "loaded " + str(self.examples)
 
 
-exit()
 # W[10*width*height] dot img[width*height] + bias[10]
-
 ts = TrainingSet()
-bias = np.zeros(ts.class_count)
-w = np.random.rand(ts.width * ts.height * ts.class_count).reshape(ts.class_count, ts.width * ts.height)
+b = np.zeros(ts.classes)
+w = np.random.rand(ts.width * ts.height * ts.classes).reshape(ts.classes, ts.width * ts.height) * 0.001
 
 
-def score(idx):
-    return w.dot(ts.get_pic(idx).flatten()) + bias
+def regularization_l2(strength):
+    return (w ** 2).dot(np.ones(ts.width * ts.height)) * strength
 
 
-def loss(scores, idx):
-    margins = np.maximum(0,scores-scores[idx]+1)
-    margins[idx] = 0
-    return np.sum(margins)
+def score(x, w):  # x is the example
+    return w.dot(x) + b
 
 
-print ts.get_label(0)
+def loss_softmax(s, label):
+    return -np.log10(s[label] / np.sum(np.exp(s)))
+
+
+def L_i(s, i):  # i is the label
+    return loss_softmax(s, i)
+
+
+def num_gradient(step):
+    gradient = w
+    for i in range(ts.classes):
+        for j in range(ts.width * ts.height):
+            print i,j
+            w_h = w
+            w_h[i, j] += step
+            gradient[i, j] = ((L(w_h) - L(w)) / step)[i]
+        print gradient
+
+
+
+def L(w):  # full loss
+    loss = 0
+    for i in range(ts.examples):
+        s = score(ts.pics[i], w)
+        li = L_i(s, ts.labels[i]) + regularization_l2(1)
+        loss += li
+    return loss / ts.examples
+
+
+num_gradient(0.001)
+
 plt.gray()
-
-s = score(0)
-print s, loss(s,0)
