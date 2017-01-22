1d7f49c1c47d625330af37f96f41fb444b16869f
FancyZero <fancyzero@live.cn>
2017-01-23 01:03:44 +0800

    compare to http://vision.stanford.edu/teaching/cs231n/linear-classify-demo/

--- SimpleClassfierVisualization.py
+++ SimpleClassfierVisualization.py
@@ -7,31 +7,43 @@ from matplotlib import cm
 fig, ax = plt.subplots()
 plt.axhline(0, color='red')
 plt.axvline(0, color='red')
-
+ax.set_xlim(-1, 1)
+ax.set_ylim(1, -1)
 classes = [0, 1, 2]
 lines = []
 for i in classes:
     l, = plt.plot([], [], 'k-', color=cm.rainbow(i / 3.0))
     lines.append(l)
 
-points = np.random.rand(20).reshape(10, 2) - 0.5
-labels = np.random.randint(0, 3, 10)
+points = np.random.rand(18).reshape(9, 2) - 0.5
+points[0]=[0.5,0.4]
+points[1]=[0.8,0.3]
+points[2]=[0.3,0.8]
+points[3]=[-0.4,0.3]
+points[4]=[-0.3,0.7]
+points[5]=[-0.7,0.2]
+points[6]=[0.7,-0.4]
+points[7]=[0.5,-0.6]
+points[8]=[-0.4,-0.5]
+
+labels = [0,0,0,1,1,1,2,2,2]
 colors = map(lambda l: cm.rainbow(l / 3.0), labels)
 
-scat = plt.scatter(points[:, 0], points[:, 1], animated=True, color=colors)
+scat = plt.scatter(points[:, 0], points[:, 1], animated=False, color=colors)
 
-examples = points
+examples = np.append(points, np.ones([len(points),1]),1)
 num_classes = len(classes)
 example_size = len(examples[0])
 
-w = np.random.rand(num_classes * example_size).reshape(num_classes, example_size)
-b = np.zeros(num_classes)
-cif = cf.Classifier(labels, examples, num_classes, w, b, 1)
+w = np.random.rand(num_classes * (example_size )).reshape(num_classes,  example_size)
+w = np.array([[1,2,0],[2,-4,0.5],[3,-1,-0.5]])
+
+cif = cf.Classifier(labels, examples, num_classes, 0.1)
 
 
 def init():
     ax.set_xlim(-1, 1)
-    ax.set_ylim(-1, 1)
+    ax.set_ylim(1, -1)
     x = lines
     x.append(scat)
     return x
@@ -39,21 +51,20 @@ def init():
 
 def update(frame):
     global w
-    global b
-    print "before"
     print w
-    gradient = cif.num_gradient(w, b, 100)
-    for wl, l, bl in zip(w, lines, b):
-        slope = wl[1] / wl[0]
-        l.set_data([1000, -1000], [slope * 1000 + bl, slope * -1000 + bl])
+    step = 0.1
+    gradient = cif.num_gradient(w)
+    print gradient
+    for wl, l in zip(w, lines):
+        slope = wl[0] / wl[1]
+        l.set_data([10, -10], [(slope * 10 - wl[2]), (slope * -10 - wl[2])])
     x = lines
     x.append(scat)
-    w += gradient * -0.001
-    print "after"
-    print w
+    w += gradient * -step
+
     return x
 
 
-ani = FuncAnimation(fig, update, frames=100,  init_func=init, blit=True)
-
+#ani = FuncAnimation(fig, update, interval=10, frames=None, init_func=init, blit=False)
+update(None)
 plt.show()
--- classifier.py
+++ classifier.py
@@ -2,19 +2,17 @@ import numpy as np
 
 
 class Classifier:
-    def __init__(self, labels, examples, num_classes, w, b, regularization_strength=1):
+    def __init__(self, labels, examples, num_classes, regularization_strength=0.1):
         self.labels = labels
         self.examples = examples
         self.num_classes = num_classes
-        self.current_w = w
-        self.current_b = b
         self.regularization_strength = regularization_strength
 
     def regularization_l2(self, w, strength):
-        return (w ** 2).dot(np.ones(w.shape[1])) * strength
+        return np.sum(w ** 2) * strength
 
-    def score(self, w, b, x):
-        return w.dot(x) + b
+    def score(self, w, x):
+        return w.dot(x)
 
     def loss_softmax(self, s, label):
         return -np.log10(np.exp(s[label]) / np.sum(np.exp(s)))
@@ -25,22 +23,25 @@ class Classifier:
     def update(self):
         pass
 
-    def num_gradient(self, w, b, step):
+    def num_gradient(self, w, h=0.00001):
         gradient = np.zeros(w.shape)
-        for i in range(self.num_classes):
-            for j in range(w.shape[1]):
-                w_h = np.copy(w)
-                w_h[i, j] += step
-                loss_w_h = self.full_loss(w_h, b)
-                loss = self.full_loss(w, b)
-                loss_gradient = ((loss_w_h - loss) / step)
-                gradient[i, j] = loss_gradient[i]
+        loss = self.full_loss(w)
+        it = np.nditer(w, flags=['multi_index'], op_flags=['readwrite'])
+        while not it.finished:
+            i = it.multi_index
+            old_value = w[i]
+            w[i] += h
+            loss_w_h = self.full_loss(w)
+            w[i] = old_value
+            loss_gradient = ((loss_w_h - loss) / h)
+            gradient[i] = loss_gradient
+            it.iternext()
         return gradient
 
-    def full_loss(self, w, b):
-        loss = np.zeros(self.num_classes)
+    def full_loss(self, w):
+        loss = 0
         for i in range(len(self.examples)):
-            s = self.score(w, b, self.examples[i])
-            li = self.loss_i(s, self.labels[i]) + self.regularization_l2(w, self.regularization_strength)
+            s = self.score(w, self.examples[i])
+            li = self.loss_i(s, self.labels[i])
             loss += li
-        return loss / len(self.examples)
+        return loss / len(self.examples) +self.regularization_l2(w, self.regularization_strength)
